30ed7db1115aedebcc58ff393027aa50870edc78
Stefano <tepo@hotmail.it>
2021-05-03 15:45:40 +0200

    Stringa censurata in maniera statica

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
-
+  $text = "Lorem ipsum dolor sit, amet consectetur adipisicing strunz. A vero aperiam error.";
+  $bad_word = "strunz";
+  $censored_text = str_replace("strunz", "***", $text);
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -10,6 +13,8 @@
     <title>PHP Badwords</title>
   </head>
   <body>
-    
+    <h1>Badwords</h1>
+    <p><?php echo $censored_text ?></p>
+    <h5>La lunghezza del paragrafo è di <?php echo strlen($censored_text) ?> caratteri</h5>
   </body>
 </html>
